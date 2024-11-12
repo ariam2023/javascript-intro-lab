761dfef1b3f0d60f347c09d88c468db5328355f8
ariam2023 <movichabraham@gmail.coml.com>
2024-11-12 13:43:06 +0000

    Javascript Lab

--- js/main.js
+++ js/main.js
@@ -45,3 +45,23 @@ defaultBtn.addEventListener("click", function () {
     document.body.setAttribute("class", "error");
 });
 
+
+
+let imageAr = ['images/view1.jpg', 'images/view2.jpg', 'images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
+document.getElementById('myImages').setAttribute('src', imageAr[2]);
+
+
+let currentIndex = 0;
+
+function chgImage(){
+    // Increment the index to get the next image
+    currentIndex++;
+      // If the index exceeds the array length, reset it to 0
+    if(currentIndex >= imageAr.length){
+        currentIndex = 0;
+    }
+    // Change the image source to the next image in the array
+    document.getElementById('myImages').setAttribute('src', imageAr[currentIndex]);
+
+    setInterval(chgImage, 2000);
+}
